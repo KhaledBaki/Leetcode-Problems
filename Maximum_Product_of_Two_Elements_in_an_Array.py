8bcc23602292134c72b74b229a6ab25715504d42
Khaled Baki <khaledbaki2018@gmail.com>
2026-07-27 00:01:32 -0400

    1464. Maximum Product of Two Elements in an Array ADDED comments
    Added a comment to explain the sorting step.

--- Maximum_Product_of_Two_Elements_in_an_Array.py
+++ Maximum_Product_of_Two_Elements_in_an_Array.py
@@ -1,4 +1,7 @@
 class Solution(object):
     def maxProduct(self, nums):
+
+        # Sorts the numbers in decreasing order
         nums = sorted(nums, reverse = True)
+
         return (nums[0] - 1) * (nums[1] - 1)
