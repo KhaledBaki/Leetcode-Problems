ea5e7baa7a7f4f6bfe20c96f554d6186699b739f
Khaled Baki <khaledbaki2018@gmail.com>
2026-08-04 22:29:53 -0400

    1929. Concatenation of Array

---
+++ Concatenation_of_Array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int size = nums.length;
+        int[] ans = new int[size * 2];
+
+        for(int i = 0; i < size; i++){
+            ans[i] = nums[i];
+            ans[size + i] = nums[i];
+        }
+        return ans;
+    }
+}
