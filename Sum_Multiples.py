f5d6f82c8abc4ae114890d72f68e9fc66980c48c
Khaled Baki <khaledbaki2018@gmail.com>
2026-08-16 23:43:49 -0400

    2652. Sum Multiples

---
+++ Sum_Multiples.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def sumOfMultiples(self, n):
+        total = 0
+
+        for i in range(1, n + 1):
+            if i % 3 == 0:
+                total += i
+
+            elif i % 5 == 0:
+                total += i
+
+            elif i % 7 == 0:
+                total += i
+                
+        return total
