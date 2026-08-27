cad9d0aea26394e3d165e877eddc4ffac9674883
Khaled Baki <khaledbaki2018@gmail.com>
2026-08-27 01:08:04 -0400

    21. Merge Two Sorted Lists

--- Merge_Two_Sorted_Lists.java
+++ Merge_Two_Sorted_Lists.java
@@ -1,63 +1,91 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 		
         ListNode cursorOne = list1;
         ListNode cursorTwo = list2;
-		ListNode mergerdList;
+		ListNode mergerdList = new ListNode();
+        ListNode mergedListCursor;
+        
 		
-        // same
+        // Null list edge cases
+        if(cursorOne == null){
+            return list2;
+        }
+        else if (cursorTwo == null){
+            return list1;
+        }
+
+        // Case 1 EQUAL case
         if(cursorOne.val == cursorTwo.val){
+
 			mergerdList = new ListNode(cursorOne.val);
-			
-			ListNode mergerdListCursor = mergerdList;
-            mergerdListCursor.next = new ListNode(cursorTwo.val);
-			
+            mergedListCursor = mergerdList;
+            mergedListCursor.next = new ListNode(cursorTwo.val);
+            
+            mergedListCursor = mergedListCursor.next;
 			cursorOne = cursorOne.next;
 			cursorTwo = cursorTwo.next;
 		}
 
-        // less than
+        // Case 2 LESS than case
 		else if(cursorOne.val < cursorTwo.val){
 			mergerdList = new ListNode(cursorOne.val);
+            mergedListCursor = mergerdList;
 			cursorOne = cursorOne.next;
 		}
 
-        // else case
+        // Case 3 ELSE case
 		else{
 			mergerdList = new ListNode(cursorTwo.val);
+            mergedListCursor = mergerdList;
 			cursorTwo = cursorTwo.next;
 		}
 		
-		while(cursorOne != null || cursorTwo != null){
+		while(cursorOne != null || cursorTwo != null){\
+
 			if(cursorOne == null){
-				mergerdListCursor.next = new ListNode(cursorTwo.val);
+				mergedListCursor.next = new ListNode(cursorTwo.val);
 				cursorTwo = cursorTwo.next;
-				mergerdListCursor = mergerdListCursor.next;
+				mergedListCursor = mergedListCursor.next;
 			}
+
 			else if(cursorTwo == null){
-				mergerdListCursor.next = new ListNode(cursorOne.val);
+				mergedListCursor.next = new ListNode(cursorOne.val);
 				cursorOne = cursorOne.next;
-				mergerdListCursor = mergerdListCursor.next;
+				mergedListCursor = mergedListCursor.next;
 			}
+
 			else if(cursorOne.val == cursorTwo.val){
-				mergerdListCursor.next = new ListNode(cursorOne.val);
+				mergedListCursor.next = new ListNode(cursorOne.val);
+                mergedListCursor = mergedListCursor.next;
+
+                mergedListCursor.next = new ListNode(cursorOne.val);
+                mergedListCursor = mergedListCursor.next;
+
 				cursorOne = cursorOne.next;
 				cursorTwo = cursorTwo.next;
-				mergerdListCursor = mergerdListCursor.next;
 			}
 
-			// less than
 			else if(cursorOne.val < cursorTwo.val){
-				mergerdListCursor.next = new ListNode(cursorOne.val);
+				mergedListCursor.next = new ListNode(cursorOne.val);
 				cursorOne = cursorOne.next;
-				mergerdListCursor = mergerdListCursor.next;
+				mergedListCursor = mergedListCursor.next;
 			}
 
-			// else case
 			else{
-				mergerdListCursor.next = new ListNode(cursorTwo.val);
+				mergedListCursor.next = new ListNode(cursorTwo.val);
 				cursorTwo = cursorTwo.next;
-				mergerdListCursor = mergerdListCursor.next;
+				mergedListCursor = mergedListCursor.next;
 			}
 		}
 		return mergerdList;
